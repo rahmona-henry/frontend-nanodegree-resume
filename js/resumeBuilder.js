c9839247bf9fdf83d3195ac72e27a9bc15253766
Someone Special <someone@devacademy.co.nz>
2016-01-14 02:01:54 +1300

    Finally got my header to work:)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var name = "Rahmona";
+var name = "Rahmona Henry";
 var formattedName = HTMLheaderName.replace("%data%",name);
 
 var role = "Web Developer";
