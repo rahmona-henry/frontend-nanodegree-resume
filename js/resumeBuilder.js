4f4e2d6ba0241797a3cb9f3446f724a793c6acfc
Someone Special <someone@devacademy.co.nz>
2016-01-14 02:27:57 +1300

    Add bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,4 +11,43 @@ $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
 
- 
+ var bio = {
+   "name" : " Rahmona Henry ",
+   "role" : " Web Developer ",
+   "contacts": {
+   "mobile" : " 111-111111 ",
+   "email" : " rahmona.henry@icloud.com ",
+   "github" : "rahmona-henry",
+   "location" : "Queenstown New Zealand"
+ },
+
+ "welcomeMessage" : "Welcome to my resume. ",
+ "skills" : [
+   "Github", "HTML", "CSS", "JS"
+ ],
+
+ "biopic": "images/fry.jpg"
+ };
+
+ if (bio.name.length) {
+
+  var formattedName = HTMLheaderName.replace("%data%",bio.name);
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+
+
+   var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+   $("#topContacts").append(formattedMobile);
+
+   var formattedEmail =HTMLemail.replace("%data%", bio.contacts.email);
+   $("#topContacts").append(formattedEmail);
+
+   var formattedLocation =HTMLlocation.replace("%data%", bio.contacts.location);
+   $("#topContacts").append(formattedLocation);
+
+   var formattedMsg =HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+   $("#header").append(formattedMsg);
+
+   var formattedPic =HTMLbioPic.replace("%data%", bio.biopic);
+   $("#header").append(formattedPic);
+};
