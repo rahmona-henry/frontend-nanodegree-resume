f759e85aab808013444ff800d2f67edc34577e50
Someone Special <someone@devacademy.co.nz>
2016-01-13 23:37:23 +1300

    Fix Udacity errors

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name = Rahmona Henry
+var formattedName= HTMLheaderName.replace("%data%", name);
 
+var role = Web Developer
+var formattedRole= HTMLheaderRole.replace("%data%", role);
 
-var name = "Rahmona Henry";
-var formattedName= HTMLheaderName.replace("%data%",name);
-
-var role = "Web Developer";
-var formattedRole= HTMLheaderRole.replace("%data%",role);
-
-$("#Header").prepend(formattedRole);
-$("#Header").prepend(formattedName);
+("#Header").append(formattedRole)
+("#Header").prepend(formattedName)
 
