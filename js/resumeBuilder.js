44b09cf0f392258372e5c4cea4039c800e081d6d
Someone Special <someone@devacademy.co.nz>
2016-01-13 22:02:31 +1300

    console.log example

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,6 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append("Rahmona Henry");
 
+var awesomeThoughts = ("I am Rahmona Henry and I am awesome")
+
+console.log (awesomeThoughts);
