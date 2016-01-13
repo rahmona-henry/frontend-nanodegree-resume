ef3f2f1f8068674699c6532cb5fe9f1fed21bafc
Someone Special <someone@devacademy.co.nz>
2016-01-13 22:18:08 +1300

    Append example

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,6 +3,10 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append("Rahmona Henry");
 
-var awesomeThoughts = ("I am Rahmona Henry and I am awesome")
+var awesomeThoughts = "I am Rahmona Henry and I am awesome"
 
 console.log (awesomeThoughts);
+
+var funThoughts = "I am Rahmona Henry and I am fun"
+var funThoughts = awesomeThoughts.replace("awesome","fun");
+$("#main").append("fun thoughts");
