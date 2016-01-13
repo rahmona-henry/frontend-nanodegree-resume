52f87b586ff5d96b43ce685508a74968f77452c4
Someone Special <someone@devacademy.co.nz>
2016-01-14 01:56:05 +1300

    Udacity

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,9 +2,11 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 var name = "Rahmona";
-var formattedName = HTMLheaderName.replace(%name%, name);
+var formattedName = HTMLheaderName.replace("%data%",name);
+
 var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace(%role%, role);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
