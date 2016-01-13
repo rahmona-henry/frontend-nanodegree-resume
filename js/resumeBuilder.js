db6650039b0fd217f5e4f5b6a98d224382c924c7
Someone Special <someone@devacademy.co.nz>
2016-01-14 00:40:54 +1300

    Error

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var name ="Rahmona"
-var formattedName = HTMLheaderName.replace(%data%, name);
+var name = "Rahmona"
+var formattedName = HTMLheaderName.replace(%data%,name);
 var role = "Web Developer"
-var formattedRole = HTMLheaderRole.replace(%data%, role);
-$("#header").prepend(formattedName);
+var formattedRole = HTMLheaderRole.replace(%data%,role);
+$("#header").append(formattedName);
 $("#header").prepend(formattedRole);
+
+
+ 
