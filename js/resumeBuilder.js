be3d943ebe44e6dd5e4962eb9488fded8374dd21
Someone Special <someone@devacademy.co.nz>
2016-01-15 12:38:46 +1300

    Add appends for bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,3 +11,29 @@ $("#header").prepend(formattedName);
 var formattedRole = HTMLheaderRole.replace("%data%",role);
 $("#header").append(formattedRole);
   
+var bio = {
+  "name" : "Rahmona Henry",
+  "role" : "Web Developer"
+  "contacts" : {
+  "mobile" : "111-111111",
+  "email" : "rahmona.henry@icloud.com",
+  "github" : "rahmona-henry",
+  "location" : "Queenstown, New Zealand"
+  },
+  "welcomeMsg" : "Welcome to my resume",
+  "skills" : ["HTML","CSS","JS","github"],
+  "bioPic" : "images/fox.jpg"
+};
+
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio.contacts);
+$("#main").append(bio.mobile);
+$("#main").append(bio.email);
+$("#main").append(bio.github);
+$("#main").append(bio.location);
+$("#main").append(bio.welcomeMsg);
+$("#main").append(bio.skills);
+$("#main").append(bio.bioPic);
+
+//var work = {position,employer,years};
