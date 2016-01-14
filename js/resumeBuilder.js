3bda08d9fecfde395a9809a3c4c56ca4971d3cbf
Someone Special <someone@devacademy.co.nz>
2016-01-15 10:44:48 +1300

    Try our prepend vs append on resume builder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,8 +6,8 @@ var name = "Rahmona Henry"
 var role = "Web Developer"
 
 var formattedName = HTMLheaderName.replace("%data%",name);
-$("#header").append(formattedName);
+$("#header").prepend(formattedName);
 
 var formattedRole = HTMLheaderRole.replace("%data%",role);
-$("#header").append(formattedRole);
+$("#header").prepend(formattedRole);
   
