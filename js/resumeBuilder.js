89b4cd50a4bb3e44e2059b4e05d8917430e72bfb
Someone Special <someone@devacademy.co.nz>
2016-01-15 14:17:02 +1300

    Fix name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -19,7 +19,7 @@ var bio = {
 };
  
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
-$("#header").prepend(formattedName);
+$("#header").append(formattedName);
 
 var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 $("#header").prepend(formattedRole);
