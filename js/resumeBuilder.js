47c7aee0f49e854fa223f0a8350fe52973eec10c
Someone Special <someone@devacademy.co.nz>
2016-01-15 14:07:02 +1300

    Add bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,39 +2,37 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-var name = "Rahmona Henry"
-var role = "Web Developer"
   
 var bio = {
   "name" : "Rahmona Henry",
-  "role" : "Web Developer"
-  "contacts" : {
-  "mobile" : "111-111111",
-  "email" : "rahmona.henry@icloud.com",
-  "github" : "rahmona-henry",
-  "location" : "Queenstown, New Zealand"
-  },
+  "role" : "Web Developer",
+  "contact" : {
+         "mobile" : "111-111111",
+         "email" : "rahmona.henry@icloud.com",
+         "github" : "rahmona-henry",
+         "location" : "Queenstown, New Zealand"
+              },
   "welcomeMsg" : "Welcome to my resume",
-  //"skills" : ["HTML","CSS","JS","github"],
+  "skills" : ["HTML","CSS","JS","github"],
   "bioPic" : "images/fox.jpg"
 };
  
-var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
 $("#header").prepend(formattedName);
 
-var formattedRole = HTMLheaderRole.replace("%data%",role);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 $("#header").append(formattedRole);
 
-var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+var formattedMobile = HTMLmobile.replace("%data%",bio.contact.mobile);
 $("#topContacts").append(formattedMobile);
 
-var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+var formattedEmail = HTMLemail.replace("%data%",bio.contact.email);
 $("#topContacts").append(formattedEmail);
 
-var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
+var formattedGithub = HTMLgithub.replace("%data%",bio.contact.github);
 $("#topContacts").append(formattedGithub);
 
-var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
+var formattedLocation = HTMLlocation.replace("%data%",bio.contact.location);
 $("#topContacts").append(formattedLocation);
 
 var formattedMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMsg);
