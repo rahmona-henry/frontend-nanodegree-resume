ca79e916f19baf97937714a9781baef0d5cf7e86
Someone Special <someone@devacademy.co.nz>
2016-01-15 14:11:43 +1300

    Fix append/prepend

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -22,7 +22,7 @@ var formattedName = HTMLheaderName.replace("%data%",bio.name);
 $("#header").prepend(formattedName);
 
 var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-$("#header").append(formattedRole);
+$("#header").prepend(formattedRole);
 
 var formattedMobile = HTMLmobile.replace("%data%",bio.contact.mobile);
 $("#topContacts").append(formattedMobile);
