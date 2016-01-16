1f1d725ad1d08e61a1c74ca287120e57c2d45e05
Someone Special <someone@devacademy.co.nz>
2016-01-16 13:11:48 +1300

    Add clicks code

--- js/helper.js
+++ js/helper.js
@@ -83,9 +83,13 @@ function logClicks(x,y) {
   );
   console.log('x location: ' + x + '; y location: ' + y);
 }
-
+// your code goes here!
 $(document).click(function(loc) {
-  // your code goes here!
+    var x = loc.pageX;
+    var y = loc.pageY;
+    
+    logClicks(x,y);
+
 });
 
 
